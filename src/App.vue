53a16da8df7e6fcac687ef7022a66909c87c4c0a
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-15 11:59:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+
+  <main>
+    <router-view></router-view>
+  </main>
+
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use "bootstrap/scss/bootstrap.scss" as *;
+@use "./styles/general.scss" as *;
+</style>
