3e52096463574590ab808afddda7ae6092030c81
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-24 10:59:30 +0100

    Created the store js file and added the first pages in the router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from "./pages/HomePage.vue";
+import RestaurantsPage from "./pages/RestaurantsPage.vue";
+import SingleRestaurantPage from "./pages/SingleRestaurantPage.vue";
+
 // todo Importare component page.components
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage,
+    },
+
+    {
+      path: "/restaurants",
+      name: "restaurants",
+      component: Restaurants,
+    },
+
+    {
+      path: "/restaurants/:slug",
+      name: "restaurant",
+      component: SingleRestaurantPage,
+    },
+  ],
 });
 export { router };
