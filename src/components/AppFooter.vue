75ab978a8f71968232c93ac21d01fd3b766e108b
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-24 10:48:59 +0100

    Created the develop branch and the header and footer components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "AppFooter",
+};
+</script>
+<template>
+  <footer></footer>
+</template>
+<style lang="scss"></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+<template>
+  <header></header>
+</template>
+<style lang="scss"></style>
