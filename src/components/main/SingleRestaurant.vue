050d2cd29b253e56ec92851da87e841e38db74c4
Paolo224 <picariellopaolo224@gmail.com>
2023-03-27 18:14:59 +0200

    add the IP address before the storage images

--- src/components/main/SingleRestaurant.vue
+++ src/components/main/SingleRestaurant.vue
@@ -19,8 +19,10 @@ export default {
 
 <template>
   <article class="my-card">
-    <img class="img-fluid"
+    <img class="img-fluid" v-if="!restaurant.img_path.includes('imgs//')"
       :src="restaurant.img_path ? restaurant.img_path : 'https://images-ext-1.discordapp.net/external/vgbhoJsJh8O-0yTSXYwtXZSPBKRTJqEKISl7nFl4h-k/https/www.lagodigarda.com/getimg/ristoranti/800/default%288%29.jpg?width=993&height=662'"
+      :alt="restaurant.img_path">
+    <img class="img-fluid" v-else :src="'http://127.0.0.1:8000/storage/' + restaurant.img_path"
       :alt="restaurant.img_path">
     <div class="card-body">
       <h5 class="card-title">{{ restaurant.name }}</h5>
