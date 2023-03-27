5c8528f37b32518bccd30288aeb758dfb0801912
Paolo224 <picariellopaolo224@gmail.com>
2023-03-27 18:36:23 +0200

    add imgUrl at store

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { reactive } from "vue";
 
 export const store = reactive({
   apiUrl: "http://127.0.0.1:8000/api/",
+  imgUrl: "http://127.0.0.1:8000/storage/",
 
   getFilePath(fileFolder, filePath, fileExtension) {
     return new URL(
