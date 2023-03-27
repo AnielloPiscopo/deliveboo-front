57a23f543f26745948a9e6285ce788d1d367a6c0
Paolo224 <picariellopaolo224@gmail.com>
2023-03-27 18:42:02 +0200

    fix in NazMenu

--- src/components/header/NavMenu.vue
+++ src/components/header/NavMenu.vue
@@ -17,31 +17,27 @@ export default {
 </script>
 
 <template>
-                <div class="col-6 d-flex buttons justify-content-end p-0 h-100">
-                        <button class="button button-header h-100 d-flex align-items-center justify-content-center">
-                            <span class="d-none d-md-inline px-1">
-                              <font-awesome-icon icon="fa-solid fa-user-plus" class="my_icon" />
-                                <a href="http://127.0.0.1:8000/register"> Registrati</a>
-                            </span>
-                        </button>
+  <div class="col-6 d-flex buttons justify-content-end p-0 h-100">
+    <button class="button button-header h-100 d-flex align-items-center justify-content-center">
+      <font-awesome-icon icon="fa-solid fa-user-plus" class="my_icon" />
+      <span class="d-none d-md-inline px-1">
+        <a href="http://127.0.0.1:8000/register"> Registrati</a>
+      </span>
+    </button>
 
-                      <button class="button button-header h-100 d-flex align-items-center justify-content-center">
-                          <font-awesome-icon icon="fa-solid fa-right-to-bracket" class="my_icon" />
-                          <span class="d-none d-md-inline px-1">
-                             <a href="http://127.0.0.1:8000/">Accedi</a>
-                          </span>
-                      </button>
-                      
-                      <button class="button button-header h-100 d-flex align-items-center justify-content-center">
-                          <font-awesome-icon icon="fa-solid fa-cart-shopping" class="my_icon" />
-                      </button>
-                  </div>
+    <button class="button button-header h-100 d-flex align-items-center justify-content-center">
+      <font-awesome-icon icon="fa-solid fa-right-to-bracket" class="my_icon" />
+      <span class="d-none d-md-inline px-1">
+        <a href="http://127.0.0.1:8000/">Accedi</a>
+      </span>
+    </button>
+
+    <button class="button button-header h-100 d-flex align-items-center justify-content-center">
+      <font-awesome-icon icon="fa-solid fa-cart-shopping" class="my_icon" />
+    </button>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-
-
-
-</style>
+<style lang="scss" scoped></style>
 
 
