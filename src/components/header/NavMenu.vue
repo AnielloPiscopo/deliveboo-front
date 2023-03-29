50da00a5b902360243b91e169cd26649d0ad1897
donatomontrone <donato.montrone@outlook.com>
2023-03-29 17:39:17 +0200

    fix header cart button

--- src/components/header/NavMenu.vue
+++ src/components/header/NavMenu.vue
@@ -40,26 +40,20 @@ export default {
 
 <template>
   <nav class="col-6 d-flex buttons justify-content-end p-0 h-100">
-    <button
-      class="button button-header h-100 d-flex align-items-center justify-content-center"
-    >
+    <button class="button button-header h-100 d-flex align-items-center justify-content-center">
       <font-awesome-icon icon="fa-solid fa-user-plus" class="my_icon" />
       <span class="d-none d-md-inline px-1">
         <a href="http://127.0.0.1:8000/register"> Registrati</a>
       </span>
     </button>
-    <button
-      class="button button-header h-100 d-flex align-items-center justify-content-center"
-    >
+    <button class="button button-header h-100 d-flex align-items-center justify-content-center">
       <font-awesome-icon icon="fa-solid fa-right-to-bracket" class="my_icon" />
       <span class="d-none d-md-inline px-1">
         <a href="http://127.0.0.1:8000/">Accedi</a>
       </span>
     </button>
-    <router-link
-      :to="{ name: 'cart' }"
-      class="button button-header h-100 d-flex align-items-center justify-content-center position-relative"
-    >
+    <router-link :to="{ name: 'cart' }"
+      class="button button-header border-0 bg-light h-100 d-flex align-items-center justify-content-center position-relative">
       <font-awesome-icon icon="fa-solid fa-cart-shopping" class="my_icon" />
       <span>{{ getQuantity() }}</span>
     </router-link>
