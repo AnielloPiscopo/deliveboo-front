95ed579a62c0c2e88fea4c18c7656198260f4fe3
donatomontrone <donato.montrone@outlook.com>
2023-04-01 21:54:45 +0200

    get dishes in category count

--- src/components/main/AppCategories.vue
+++ src/components/main/AppCategories.vue
@@ -6,6 +6,7 @@ export default {
     data() {
         return {
             store,
+            count: '',
         }
     },
 
@@ -14,38 +15,40 @@ export default {
             type: Array,
             required: true,
         },
+        dishes: {
+            type: Array,
+            required: true,
+        },
+    },
+
+    methods: {
+        getCount(id) {
+            return this.dishes.filter(dish => dish.category_id === id).length;
+        }
     },
 }
 </script>
 
 <template>
-
-    
-
-        <nav id="sidebar">
-            <div class="p-2 mt-4">
-                    <h4 class="pb-3">Categorie</h4>
-                <ul class="list-unstyled components" v-for="(category) in categories">
-                    <li><h6>{{ category.title }}</h6></li>
-                </ul>
-            </div>
-        </nav>
-
-
-
-    
+    <nav id="sidebar">
+        <div class="p-2 mt-4">
+            <h4 class="pb-3">Categorie</h4>
+            <ul class="list-unstyled components" v-for="(category) in categories">
+                <li>
+                    <h6>{{ category.title }} ({{ getCount(category.id) }})</h6>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <style scoped lang="scss">
-
-#sidebar{
+#sidebar {
 
     min-width: 270px;
     max-width: 270px;
     transition: all .3s;
     position: relative;
-    border-left: 1px solid rgba(0,0,0,.05);
+    border-left: 1px solid rgba(0, 0, 0, .05);
 }
-
-
 </style>
